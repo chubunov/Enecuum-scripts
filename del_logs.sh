f6dfe19f6745f7eaec446dff7b7fcd89de2e93d6
chubunov <114956634+chubunov@users.noreply.github.com>
2023-11-18 21:51:20 +0300

    Add files via upload

---
+++ del_logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+  echo "Выберите какие логи удалить:"
+  echo "1. Очистите логи приложения PoS узла"
+  echo "2. Очистите логи Docker-контейнеров"
+  echo "3. Отмена"
+
+  read choice
+
+  case $choice in
+    1)
+      sudo docker exec pulse_pos pm2 flush
+      ;;
+    2)
+      sudo truncate -s 0 /var/lib/docker/containers/*/*-json.log;
+      ;;
+    0) exit ;;
+   
+  esac
+
