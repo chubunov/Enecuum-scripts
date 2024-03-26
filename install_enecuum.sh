2de7488a5258a32632a3b4046d13987897dae358
chubunov <114956634+chubunov@users.noreply.github.com>
2024-03-26 21:15:13 +0300

    new IP

--- install_enecuum.sh
+++ install_enecuum.sh
@@ -7,7 +7,7 @@ read -p "Введите iD POS контракта: " contract_id
 echo
 echo
 db_command="sudo docker run -d --name pulse_db -e MYSQL_ROOT_PASSWORD=${password} enecuum/pulse_db"
-pos_command="sudo docker run -ti --name pulse_pos --link pulse_db:dbhost -p8000:8000 -e POS_ID=${contract_id} -e PORT=8000 -e DB_PASS=${password} -e PEER='95.216.68.221:8000' -e DB_PORT=3306 -e LAG_INTERVAL=300 -e SYNC_INTERVAL=258 -d enecuum/pulse_pos"
+pos_command="sudo docker run -ti --name pulse_pos --link pulse_db:dbhost -p8000:8000 -e POS_ID=${contract_id} -e PORT=8000 -e DB_PASS=${password} -e PEER='65.109.66.179:8000' -e DB_PORT=3306 -e LAG_INTERVAL=300 -e SYNC_INTERVAL=258 -d enecuum/pulse_pos"
 
 eval "$db_command"
 eval "$pos_command"
