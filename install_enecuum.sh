a00620f6d5b9cf193588975ae7c8a56b8e9768fc
chubunov <114956634+chubunov@users.noreply.github.com>
2024-03-28 22:55:05 +0300

    new POS update

--- install_enecuum.sh
+++ install_enecuum.sh
@@ -6,10 +6,14 @@ echo
 read -p "Введите iD POS контракта: " contract_id
 echo
 echo
+read -p "Введите публичный ключ POS аккаунта: " POS_OWNER_PUB
+echo
+echo
+read -p "Введите приватный ключ POS аккаунта: " POS_OWNER_PRV
+
+
 db_command="sudo docker run -d --name pulse_db -e MYSQL_ROOT_PASSWORD=${password} enecuum/pulse_db"
-pos_command="sudo docker run -ti --name pulse_pos --link pulse_db:dbhost -p8000:8000 -e POS_ID=${contract_id} -e PORT=8000 -e DB_PASS=${password} -e PEER='65.109.66.179:8000' -e DB_PORT=3306 -e LAG_INTERVAL=300 -e SYNC_INTERVAL=258 -d enecuum/pulse_pos"
+pos_command="sudo docker run -ti --name pulse_pos --link pulse_db:dbhost -p8000:8000 -p3000:3000 -p80:80 -e POS_ID=${contract_id} -e PORT=8000 -e DB_PASS=${password} -e PEER='65.109.66.179:8000' -e DB_PORT=3306 -e POS_OWNER_PUB=${POS_OWNER_PUB} -e POS_OWNER_PRV=${POS_OWNER_PRV} -e POA_PORT=3000 -e EXPLORER_PORT=80  -e LAG_INTERVAL=300 -e SYNC_INTERVAL=258  -d enecuum/pulse_pos:latest"
 
 eval "$db_command"
 eval "$pos_command"
-
-
